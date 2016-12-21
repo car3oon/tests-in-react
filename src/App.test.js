baf8b9a5d7bbf90380dbba130e7e8db677a9ab05
Łukasz Węgiel <lukasz@mirumee.com>
2016-12-21 11:58:53 +0100

    Use enzyme for tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App, {sum} from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h3>Tests in React</h3>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
 
 it('sums numbers', () => {
